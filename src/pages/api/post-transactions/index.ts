059cfaeec68eeb15ad7165230877e5cfbb6f3cd6
brendenozie <brendenozie@gmail.com>
2024-09-27 01:26:28 +0300

    updated transaction table and post

--- src/pages/api/post-transactions/index.ts
+++ src/pages/api/post-transactions/index.ts
@@ -6,17 +6,33 @@ import prisma from "../../../server/db/prismadb";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-        const { userId, subscriptionPlanId, amount, currency, startingAt, endingAt } = req.body;
+        const { userId, subscriptionPlanId, amount, status, currency, startingAt, endingAt } = req.body;
 
         try {
             // Create new transaction
+            // {"amount":34.99,"currency":"USD","endingAt":"2024-12-26T20:34:08.214038Z","id":"",
+            //     "startingAt":"2024-12-26T20:34:08.212153Z","status":"active","subscriptionPlanId":"66f46ffe775c11ec67560278",
+            //     "userId":"66cb2206577e7774f63c97bf"}
+            if (!userId || !subscriptionPlanId || !amount || !currency || !status || !startingAt || !endingAt) {
+                return res.status(400).json({ message: 'Please provide query parameters' });
+            }
+
+            if (typeof startingAt !== 'string' || typeof endingAt !== 'string') {
+                return res.status(400).json({ message: 'Please provide fromDate and toDate as strings' });
+            }
+
+            const tareheStart = new Date(startingAt);
+            const tareheEnd = new Date(endingAt);
+
             const transaction = await prisma.transaction.create({
                 data: {
                     amount,
                     currency,
-                    status: 'pending', // Update status after payment confirmation
+                    status, // Update status after payment confirmation
                     user: { connect: { id: userId } },
-                    subscriptionPlan: { connect: { id: subscriptionPlanId } }
+                    subscriptionPlan: { connect: { id: subscriptionPlanId } },
+                    startingAt: tareheStart,
+                    endingAt:tareheEnd
                 }
             });
 
